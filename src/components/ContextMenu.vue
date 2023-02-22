983f6351fa5e5e16a637379eae6516ac25aa7880
Denis Borsukov <denys.borsukov@gmail.com>
2023-02-22 11:38:19 +0200

    custom context menu

---
+++ src/components/ContextMenu.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref, type Ref } from 'vue';
+
+const props = defineProps({
+  menuId: { type: String, required: true },
+  options: { type: Array<String>, required: true },
+});
+
+const emit = defineEmits(['handle-option']);
+
+defineExpose({ open });
+
+const active = ref(false);
+
+const item = ref();
+
+function useClickOutside(target_ref: Ref<HTMLElement>, callback: Function) {
+  if (!target_ref) return;
+
+  let listener = (e: MouseEvent) => {
+    if (e.target == target_ref.value || e.composedPath().includes(target_ref.value)) {
+      return;
+    }
+    callback();
+  };
+
+  onMounted(() => {
+    window.addEventListener('click', listener);
+  });
+  onBeforeUnmount(() => {
+    window.removeEventListener('click', listener);
+  });
+}
+
+const menu = ref();
+
+useClickOutside(menu, hide);
+
+function open(event: MouseEvent, it: any) {
+  item.value = it;
+
+  let menu = document.getElementById(props.menuId);
+  if (!menu) return;
+
+  menu.style.left = event.pageX + 'px';
+  menu.style.top = event.pageY + 'px';
+
+  active.value = true;
+}
+
+function hide() {
+  const menu = document.getElementById(props.menuId);
+  if (menu) {
+    active.value = false;
+  }
+}
+
+function optionClicked(op: String) {
+  emit('handle-option', op, item.value);
+  hide();
+}
+
+function onEscPressed(event: KeyboardEvent) {
+  if (event.code == 'Escape') {
+    hide();
+  }
+}
+
+onMounted(() => {
+  document.body.addEventListener('keyup', onEscPressed);
+});
+
+onBeforeUnmount(() => {
+  document.body.removeEventListener('keyup', onEscPressed);
+});
+</script>
+
+<template>
+  <div :id="menuId" ref="menu" v-show="active" class="absolute z-50 w-40">
+    <div
+      class="flex flex-col gap-y-2 rounded-lg border bg-white p-2 shadow-lg dark:border-neutral-700 dark:bg-neutral-800"
+    >
+      <div v-for="(op, index) in options" :key="index" @click.stop="optionClicked(op)">
+        <button
+          class="w-full rounded-md px-2 py-1 text-left text-sm text-black hover:bg-neutral-200 active:bg-neutral-300 dark:text-neutral-50 dark:hover:bg-neutral-700 dark:active:bg-neutral-900"
+        >
+          {{ op }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
